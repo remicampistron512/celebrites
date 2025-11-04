d16cfba4b0f887dfc19959a2a439ef0eb87c73c5
Rémi Campistron <remi.campistron@gmail.com>
2025-11-04 19:48:49 +0100

    Detecte les celebrités

--- src/celebrites/Celebrities.java
+++ src/celebrites/Celebrities.java
@@ -1,26 +1,28 @@
 
 package celebrites;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 
 public class Celebrities {
-	public static void main(String[] args) {
-		String[] guestsList = {"Albert","Benedicte","CHristophe","Delphine","Edouard","Francoise","Gaston","Heloise"};
-		Map <Integer, int []> guestRelations = new HashMap<Integer, int[]>();
-		Map <Integer, boolean []> guestsKnowledge = new HashMap <Integer, boolean[]>();
-		
-        guestRelations.put(0, new int[]{1,4,5});          
-        guestRelations.put(1, new int[]{2,4,5});           
-        guestRelations.put(2, new int[]{1,3,4,5});         
-        guestRelations.put(3, new int[]{0,1,4,5,7});       
-        guestRelations.put(4, new int[]{5});              
-        guestRelations.put(5, new int[]{4});              
-        guestRelations.put(6, new int[]{1,2,4,5,7});      
-        guestRelations.put(7, new int[]{1,3,4,5,6});
-        
+    public static void main(String[] args) {
+        String[] guestsList = {"Albert", "Benedicte", "CHristophe", "Delphine", "Edouard", "Francoise", "Gaston", "Heloise"};
+        Map<Integer, int[]> guestRelations = new HashMap<Integer, int[]>();
+        Map<Integer, boolean[]> guestsKnowledge = new HashMap<Integer, boolean[]>();
+
+        guestRelations.put(0, new int[]{1, 4, 5});
+        guestRelations.put(1, new int[]{2, 4, 5});
+        guestRelations.put(2, new int[]{1, 3, 4, 5});
+        guestRelations.put(3, new int[]{0, 1, 4, 5, 7});
+        guestRelations.put(4, new int[]{5,1});
+        guestRelations.put(5, new int[]{4});
+        guestRelations.put(6, new int[]{1, 2, 4, 5, 7});
+        guestRelations.put(7, new int[]{1, 3, 4, 5, 6});
+
         guestsKnowledge.put(0, new boolean[8]);
         guestsKnowledge.put(1, new boolean[8]);
         guestsKnowledge.put(2, new boolean[8]);
@@ -29,80 +31,73 @@ public class Celebrities {
         guestsKnowledge.put(5, new boolean[8]);
         guestsKnowledge.put(6, new boolean[8]);
         guestsKnowledge.put(7, new boolean[8]);
-        
-        
+
+
         //System.out.println(getCelebrities(guestRelations,guestsKnowledge));
-        
-        isKnownByEveryOne(4,guestRelations,guestsKnowledge);
-        
-        guestsKnowledge.get(4);
-        
-        for (boolean isKnown : guestsKnowledge.get(4)) {
-        	System.out.println(isKnown);
-        	
+        for (Entry<Integer, boolean[]> guest : guestsKnowledge.entrySet()) {
+            getKnowers(guest.getKey(), guestRelations, guestsKnowledge);
         }
-        
-        
-     
-		
-	}
-	
-	
 
-	private static Map <Integer, boolean []> isKnownByEveryOne(int guestId,Map<Integer, int[]> guestRelations, Map <Integer, boolean []> guestsKnowledge ) {
-		// On boucle sur les invités
-		for (Entry<Integer, int []> relation : guestRelations.entrySet()) {
-				// On s'assure de ne pas boucler sur l'invité que l'on traite en param de la methode
-			 	if(relation.getKey() != guestId) {
-			 		// Pour chaque invité on recupere la liste des invités qu'il connait
-					int[]knownGuests = relation.getValue();
-					
-						
-						boolean[] arr = new boolean[8];
-						arr = guestsKnowledge.get(relation.getKey());
-						for (int i=0; i<knownGuests.length; i++) {
-							 
-							
-							
-							if(knownGuests[i] == guestId) {									
-								arr[knownGuests[i]] = true;
-								guestsKnowledge.put(guestId, arr);
-							}							
-						
-						}
-							
-			}
-			 	
-		}
-		return guestsKnowledge;
-	}
+        for (Entry<Integer, boolean[]> guest : guestsKnowledge.entrySet()) {
+            System.out.println(Arrays.toString(guest.getValue()));
 
-
-
-	public static ArrayList <Integer> getCelebrities(Map<Integer, int[]> guestRelations, Map<Integer,boolean[]> guestsKnowledge) {
-		Boolean isKnown = false;
-		Boolean isKnownByAll = false;
-		ArrayList <Integer> celebList = new ArrayList<Integer>();
-		for (Entry<Integer, int []> relation : guestRelations.entrySet()) {					
-        	 int[] knownGuests = relation.getValue();
-        	 for (Entry<Integer, int []> relation2 : guestRelations.entrySet()){
-        		 isKnown = false;
-        		 if(relation.getKey() != relation2.getKey()) {
-        			 for (int i=0; i<knownGuests.length; i++) {
-        				 if(knownGuests[i] == relation2.getKey()) {
-        					 isKnown = true;
-        					 break;
-        				 }        				
-        			 }
-        		 }
-        		 
-        	 }
-        	 if(isKnown) isKnownByAll = true;
-        	 if(isKnownByAll == true) celebList.add(relation.getKey());
         }
-		return celebList;
-	}
-	
-	
+        System.out.println(getCelebrities(guestsKnowledge));
+
+    }
+
+    private static ArrayList<Integer> getCelebrities(Map<Integer, boolean[]> guestsKnowledge) {
+        /* Si la personne est connu de tous, c'est une célébrité donc on parcourt la hashmap guestsKnowledge pour rechercher les tableaux complètement true. */
+        ArrayList<Integer> celebritiesList = new ArrayList<>();
+        for(Entry<Integer, boolean[]> guest : guestsKnowledge.entrySet()) {
+            int guestId = guest.getKey();
+            boolean[] knownBy = guest.getValue();
+
+            boolean allKnown = true;
+            for (boolean known : knownBy) {
+                if (!known) {
+                    allKnown = false;
+                    break;
+                }
+            }
+
+            if (allKnown) {
+                celebritiesList.add(guestId);
+            }
+        }
+
+        return celebritiesList;
+
+
+    }
+
+
+    private static void getKnowers(int guestId, Map<Integer, int[]> guestRelations, Map<Integer, boolean[]> guestsKnowledge) {
+        // On boucle sur les invités
+        boolean[] arr = guestsKnowledge.getOrDefault(guestId, new boolean[8]);
+        for (Entry<Integer, int[]> relation : guestRelations.entrySet()) {
+            int knower = relation.getKey();
+            // On s'assure de ne pas boucler sur l'invité que l'on traite en param de la methode
+            if (knower == guestId){
+                arr[knower] = true;
+                continue;
+            }
+            // Pour chaque invité, on récupère la liste des invités qu'il connait
+            int[] knownGuests = relation.getValue();
+
+            for (int knownGuest : knownGuests) {
+                if (knownGuest == guestId) {
+                    System.out.println(knownGuest);
+                    arr[knower] = true;
+                    break;
+                }
+            }
+        }
+        guestsKnowledge.put(guestId, arr);
+    }
+
+
+
+
 }
 
