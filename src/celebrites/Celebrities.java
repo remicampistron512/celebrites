3191bba7fceb3f0c5b22b9b2d556905034877ec9
Rémi Campistron <remi.campistron@gmail.com>
2025-11-08 10:52:33 +0100

    Reformatage du code avec checkstyle

--- src/celebrites/Celebrities.java
+++ src/celebrites/Celebrities.java
@@ -4,16 +4,15 @@ package celebrites;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.HashMap;
 
 
 public class Celebrities {
-    public static void main(String[] args) {
-        String[] guestsList = {"Albert", "Benedicte", "CHristophe", "Delphine", "Edouard", "Francoise", "Gaston", "Heloise"};
-        
+    public static void main(final String[] args) {
+        final String[] guestsList = {"Albert", "Benedicte", "CHristophe", "Delphine", "Edouard", "Francoise", "Gaston", "Heloise"};
+
         // hashmap contenant les connaisances de chaque invité
-        Map<Integer, int[]> guestRelations = new HashMap<Integer, int[]>();
+        final Map<Integer, int[]> guestRelations = new HashMap<Integer, int[]>();
         guestRelations.put(0, new int[]{1, 4, 5});
         guestRelations.put(1, new int[]{2, 4, 5});
         guestRelations.put(2, new int[]{1, 3, 4, 5});
@@ -23,38 +22,39 @@ public class Celebrities {
         guestRelations.put(6, new int[]{1, 2, 4, 5, 7});
         guestRelations.put(7, new int[]{1, 3, 4, 5, 6});
 
-        /* hashmap determinant si l'invité est connu ou pas de l'invité correspondant. 
+        /* hashmap determinant si l'invité est connu ou pas de l'invité correspondant.
          * l'indice du tableau de booleen correspond à l'indice du tableau des invités
-         *  (ex: true en 3eme pos signifie que le 3eme invité le connait 
+         *  (ex: true en 3eme pos signifie que le 3eme invité le connait
          */
-      
-        int n = guestsList.length;
-        Map<Integer, boolean[]> guestsKnowledge = new HashMap<>(n);
+
+        final int n = guestsList.length;
+        final Map<Integer, boolean[]> guestsKnowledge = new HashMap<>(n);
         for (int i = 0; i < n; i++) {
             guestsKnowledge.put(i, new boolean[n]);
         }
-        
-        for (Entry<Integer, boolean[]> guest : guestsKnowledge.entrySet()) {
-            getKnowers(guest.getKey(), guestRelations, guestsKnowledge);
+
+        for (final Map.Entry<Integer, boolean[]> guest : guestsKnowledge.entrySet()) {
+            Celebrities.getKnowers(guest.getKey(), guestRelations, guestsKnowledge);
         }
 
-        for (Entry<Integer, boolean[]> guest : guestsKnowledge.entrySet()) {
+        for (final Map.Entry<Integer, boolean[]> guest : guestsKnowledge.entrySet()) {
             System.out.println(Arrays.toString(guest.getValue()));
 
         }
-        System.out.println(getCelebrities(guestsKnowledge));
+        System.out.println(Celebrities.getCelebrities(guestsKnowledge));
 
     }
 
-    private static ArrayList<Integer> getCelebrities(Map<Integer, boolean[]> guestsKnowledge) {
-        /* Si la personne est connue de tous, c'est une célébrité donc on parcourt la hashmap guestsKnowledge pour rechercher les tableaux complètement true. */
-        ArrayList<Integer> celebritiesList = new ArrayList<>();
-        for(Entry<Integer, boolean[]> guest : guestsKnowledge.entrySet()) {
-            int guestId = guest.getKey();
-            boolean[] knownBy = guest.getValue();
+    private static ArrayList<Integer> getCelebrities(final Map<Integer, boolean[]> guestsKnowledge) {
+        /* Si la personne est connue de tous, c'est une célébrité donc on parcourt la hashmap
+         guestsKnowledge pour rechercher les tableaux complètement true. */
+        final ArrayList<Integer> celebritiesList = new ArrayList<>();
+        for(final Map.Entry<Integer, boolean[]> guest : guestsKnowledge.entrySet()) {
+            final int guestId = guest.getKey();
+            final boolean[] knownBy = guest.getValue();
 
             boolean allKnown = true;
-            for (boolean known : knownBy) {
+            for (final boolean known : knownBy) {
                 if (!known) {
                     allKnown = false;
                     break;
@@ -72,20 +72,20 @@ public class Celebrities {
     }
 
 
-    private static void getKnowers(int guestId, Map<Integer, int[]> guestRelations, Map<Integer, boolean[]> guestsKnowledge) {
+    private static void getKnowers(final int guestId, final Map<Integer, int[]> guestRelations, final Map<Integer, boolean[]> guestsKnowledge) {
         // On boucle sur les invités
-        boolean[] arr = guestsKnowledge.getOrDefault(guestId, new boolean[8]);
-        for (Entry<Integer, int[]> relation : guestRelations.entrySet()) {
-            int knower = relation.getKey();
+        final boolean[] arr = guestsKnowledge.getOrDefault(guestId, new boolean[8]);
+        for (final Map.Entry<Integer, int[]> relation : guestRelations.entrySet()) {
+            final int knower = relation.getKey();
             // On s'assure de ne pas boucler sur l'invité que l'on traite en param de la methode
             if (knower == guestId){
                 arr[knower] = true;
                 continue;
             }
             // Pour chaque invité, on récupère la liste des invités qu'il connait
-            int[] knownGuests = relation.getValue();
+            final int[] knownGuests = relation.getValue();
 
-            for (int knownGuest : knownGuests) {
+            for (final int knownGuest : knownGuests) {
                 if (knownGuest == guestId) {
                     System.out.println(knownGuest);
                     arr[knower] = true;
