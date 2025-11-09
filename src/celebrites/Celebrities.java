74c751f8caab5cf5baaa7aa0a66a85e30a3fdfc5
Rémi Campistron <remi.campistron@gmail.com>
2025-11-09 11:10:14 +0100

    Implemente la recursivité dand la méthode onlyKnowsCelebrities fixe #6

--- src/celebrites/Celebrities.java
+++ src/celebrites/Celebrities.java
@@ -33,7 +33,7 @@ public class Celebrities {
     guestRelations.put(1, new int[] {2, 4, 5});
     guestRelations.put(2, new int[] {1, 3, 4, 5});
     guestRelations.put(3, new int[] {0, 1, 4, 5, 7});
-    guestRelations.put(4, new int[] {5, 1});
+    guestRelations.put(4, new int[] {5,1});
     guestRelations.put(5, new int[] {4});
     guestRelations.put(6, new int[] {1, 2, 4, 5, 7});
     guestRelations.put(7, new int[] {1, 3, 4, 5, 6});
@@ -69,20 +69,45 @@ public class Celebrities {
 
   private static ArrayList<Integer> onlyKnowCelebrities(
       ArrayList<Integer> celebritiesCandidates, Map<Integer, int[]> guestRelations) {
-    final ArrayList<Integer> celebritiesList = new ArrayList<>();
 
-    for (int candidate : celebritiesCandidates) {
-      int[] knownList = guestRelations.get(candidate);
-      for (int known : knownList) {
-        for (int candidate2 : celebritiesCandidates) {
-          if (known != candidate2 || known == candidate) {
-            break;
+    boolean changed;
+    // boucle de recursion
+    do {
+      // flag pour savoir si la liste a été changé
+      changed = false;
+      // le tableau des invités que l'on va enlever de la liste a chaque iteration
+      final ArrayList<Integer> toRemove = new ArrayList<>();
+      // on parcourt la liste des candidats
+      for (int candidate : celebritiesCandidates) {
+        // récupère les relations du candidat
+        int[] knownList = guestRelations.get(candidate);
+        // flag pour determine s'il ne connait que des candidats
+        boolean onlyKnowsCandidates = true;
+        for (int known : knownList) {
+          // si la connaissance n'est pas dans la liste alors cette connaissance
+          // n'est pas une célébrité, dès lors le candidat n'est plus une célébrité
+          // la connaissance est un candidat, on passe à la prochaine iteration
+          if (known != candidate && !celebritiesCandidates.contains(known)) {
+            onlyKnowsCandidates = false;
+            break; // single control-flow statement
           }
-          celebritiesList.add(candidate);
+
+        } if (!onlyKnowsCandidates) {
+          // le candidat ne connait pas que des célébrités, il est ajouté à toRemove
+          // pour être retiré par la suite
+          toRemove.add(candidate);
         }
       }
-    }
-    return celebritiesList;
+
+      // On vide la liste des candidats si il y a toujours des candidats
+      if (!toRemove.isEmpty()) {
+        celebritiesCandidates.removeAll(toRemove);
+        // on reboucle vu que la liste de candidats a changé, il faut revérifier
+        changed = true;
+      }
+    } while (changed);
+
+    return celebritiesCandidates;
   }
 
   private static ArrayList<Integer> getCelebritiesCandidates(
